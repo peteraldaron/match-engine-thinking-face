6287e59b430d3702c3380a22d16a5e691662f62d
Peter Zhang <peter.zhang@reaktor.com>
2018-05-03 00:20:54 +0300

    more filters

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from concurrent.futures import ThreadPoolExecutor
 
 logger = logging.getLogger()
 BASE_URL = 'https://api.gotinder.com'
+DESIGNATED_SCHOOLS = ['aalto', 'helsinki', 'åbo', 'lund', 'stock', 'oulu', 'sibeliu']
+can_approve = True
 
 
 with open('config.json') as fp:
@@ -19,21 +21,49 @@ def get_headers():
             'User-Agent': conf.get('ua'),
             'Content-Type': 'application/json'}
 
+def has_designated_school(school_obj_list):
+    schools = ''.join([ob.get('name', '') for ob in school_obj_list]).lower()
+    return any((school in schools for school in DESIGNATED_SCHOOLS))
+
 
 def filtering_criteria(user_profile):
-    return (len(user_profile.get('bio')) != 0
+    return (len(user_profile.get('bio')) > 3
             and len(user_profile.get('photos')) > 1
             and (len(user_profile.get('jobs')) > 0 or (len(user_profile.get('schools')) > 0))
-            and user_profile.get('gender') == 1)
+            and user_profile.get('gender') == 1) or has_designated_school(user_profile.get('schools', []))
+
 
 def filter_user(user_profile):
+    global can_approve
+    def _make_request_keep():
+        return requests.get(f'{BASE_URL}/like/{user_profile.get("_id")}', headers=get_headers())
+
+    def _make_request_filter():
+        return requests.get(f'{BASE_URL}/pass/{user_profile.get("_id")}', headers=get_headers())
+
     if not filtering_criteria(user_profile):
         print(f'filtering out user {user_profile.get("_id")}')
-        requests.get(f'{BASE_URL}/pass/{user_profile.get("_id")}')
+        res = _make_request_filter()
+        while res.status_code != 200:
+            time.sleep(1)
+            res = _make_request_filter()
+        print(f'user {user_profile.get("_id")} filter query returned: {res.status_code}')
         return {}
     else:
-        requests.get(f'{BASE_URL}/like/{user_profile.get("_id")}')
+        if not can_approve:
+            return {}
+
+        res = _make_request_keep()
+        while res.status_code != 200:
+            time.sleep(1)
+            res = _make_request_keep()
+        if not res.json().get('likes_remaining', False):
+            print(f'Out of likes, recording negatives')
+            can_approve = False
+            return {}
+        print(f'user {user_profile.get("_id")} keep query returned: {res.status_code}\n{res.json()}')
         return user_profile
+
 
 def is_match(user_profile):
     return user_profile.get('group_matched') is True
@@ -45,6 +75,7 @@ def sanitize_result(user_profile):
     for key in keys:
         if key not in allowed_keys:
             del user_profile[key]
+    return user_profile
 
 def get_recs():
     try:
@@ -57,7 +88,7 @@ def get_recs():
         response = [v for v in response if len(v)]
 
         if len(response):
-            with open(str(datetime.datetime.utcnow()) + '.json', 'w') as fp:
+            with open('-'.join(str(datetime.datetime.utcnow()).split(' ')) + '.json', 'w') as fp:
                 fp.write(json.dumps([sanitize_result(res) for res in response]))
         print('wrote batch')
 
